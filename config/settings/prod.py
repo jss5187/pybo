3878066b23b2e0ac0ed1475cdbef3c26347c919b
정성산 <jss5187@naver.com>
2022-08-28 16:18:43 +0900

    domain register

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.164.143.223']
+ALLOWED_HOSTS = ['15.164.143.223', 'devsan2.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
